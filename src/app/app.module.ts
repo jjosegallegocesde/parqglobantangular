d872d4fac406397de4d49a2b01957247bcb17c37
jjosegallegomesa <juan.gallegomesa@gmail.com>
2022-07-17 10:04:05 -0500

    se crea parq

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { VehiculosComponent } from './vehiculos/vehiculos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    VehiculosComponent
   ],
   imports: [
     BrowserModule,
